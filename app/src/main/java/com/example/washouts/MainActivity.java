b299498b74f5964fd86e9b052fc0b5ec6c2200c8
Dxrsh <egedrshn123@gmail.com>
2024-01-20 12:47:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/washouts/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.washouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(() -> {
+//            if(FireBase.getCurrentUserId() !=null){
+//                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+//                startActivity(intent);
+//            }else {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+//            }
+            finish();
+        },1500);
+    }
+}
